63d2fb2dd172d3fea42db2eee9cfe7deffa63907
Paulo Henrique <ph.1928@hotmail.com>
2024-07-01 13:40:08 -0300

    Finaliza exercício 2

--- src/Exercicio2/Main.java
+++ src/Exercicio2/Main.java
@@ -2,6 +2,15 @@ package Exercicio2;
 
 public class Main {
     public static void main(String[] args) {
+        Produto mesa = new Produto("Mesa", 150, 10 );
 
+        mesa.exibirDetalhes();
+
+        for (int i = 0; i < 15; i++) {
+            System.out.println("Comprando produto...");
+            mesa.atualizarEstoque();
+            mesa.exibirDetalhes();
+            System.out.println("================");
+        }
     }
 }
--- src/Exercicio2/Produto.java
+++ src/Exercicio2/Produto.java
@@ -3,6 +3,49 @@ package Exercicio2;
 public class Produto {
     private String nome;
     private double preco;
-    private int quantidade;
-    private int estoque;
+    private int quantidadeEmEstoque;
+
+    public Produto(String nome, double preco, int quantidadeEmEstoque) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidadeEmEstoque() {
+        return this.quantidadeEmEstoque;
+    }
+
+    public void setQuantidadeEmEstoque(int quantidade) {
+        this.quantidadeEmEstoque = quantidade;
+    }
+
+    public void atualizarEstoque() {
+        if (this.quantidadeEmEstoque <= 0) {
+            System.out.println("Produto fora de estoque");
+            return;
+        }
+
+        this.setQuantidadeEmEstoque(this.getQuantidadeEmEstoque() - 1);
+        System.out.println("Compra efetuada com sucesso");
+    }
+
+    public void exibirDetalhes() {
+        System.out.println("Nome: " + this.getNome() + "\nPreço: " + this.getPreco() + "\nQuantidade: " + this.getQuantidadeEmEstoque());
+    }
 }
