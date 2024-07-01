67ea27776d1aa67b676e4c0abff19f4dfdf391c7
Paulo Henrique <ph.1928@hotmail.com>
2024-07-01 15:11:21 -0300

    Finaliza exercício 4

--- src/Exercicio4/Funcionario.java
+++ src/Exercicio4/Funcionario.java
@@ -1,4 +1,58 @@
 package Exercicio4;
 
 public class Funcionario {
+    private String nome;
+    private String cargo;
+    private double salario;
+    private String departamento;
+
+    public Funcionario(String nome, String cargo, double salario, String departamento) {
+        this.nome = nome;
+        this.cargo = cargo;
+        this.salario = salario;
+        this.departamento = departamento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public void exibirDetalhes() {
+        System.out.println("Nome: " + this.getNome() + "\nCargo: " + this.getCargo() +
+                "\nSalário: " + this.getSalario() + "\nDepartamento: " + this.getDepartamento());
+    }
+
+    public void promover(String novoCargo, double novoSalario, String novoDepartamento) {
+        this.setCargo(novoCargo);
+        this.setSalario(novoSalario);
+        this.setDepartamento(novoDepartamento);
+    }
 }
--- src/Exercicio4/Main.java
+++ src/Exercicio4/Main.java
@@ -2,6 +2,16 @@ package Exercicio4;
 
 public class Main {
     public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario("João Martins", "Assistente", 2000, "RH");
 
+        funcionario.exibirDetalhes();
+        System.out.println("===========");
+
+        System.out.println("Promovendo funcionário...");
+        funcionario.promover("Analista", 3500, "TI");
+        System.out.println("Funcionário promovido!");
+
+        System.out.println("===========");
+        funcionario.exibirDetalhes();
     }
 }
